f7349562554018b3cca0e7340bee7748ce7b31cb
Tony Tran <tuan.tran@metacrew.vn>
2025-12-16 15:02:18 +0700

    update docker file

--- patching-script.sh
+++ patching-script.sh
@@ -19,9 +19,3 @@ cp fromai-public/scripts/replace-image-cdn-handler.js scripts/replace-image-cdn-
 
 # Dockerfile
 cp fromai-public/Dockerfile Dockerfile
-sed -i "s|{BASE44_URL}|${{ env.BASE44_URL }}|g" Dockerfile
-sed -i "s|{PROJECT_KEY}|${{ env.PROJECT_KEY }}|g" Dockerfile
-sed -i "s|{environment}|${{ env.ENV == 'prod' && 'production' || 'development' }}|g" Dockerfile
-
-sed -i '1i import "./auth-sdk/index";' src/main.jsx
-sed -i "s|VITE_BASE44_BACKEND_URL|${{ env.API_URL }}|g" public/callback.js
