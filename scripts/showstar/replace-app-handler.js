fa45cc4414a2c785d0e0378dfa400e3e381e48a4
Joi(FD) <joi.nguyen@metacrew.vn>
2025-12-23 15:25:32 +0700

    update check

--- scripts/showstar/replace-app-handler.js
+++ scripts/showstar/replace-app-handler.js
@@ -44,12 +44,12 @@ const checkAppLogin = `
                 };
                 localStorage.setItem("superAdminSession", JSON.stringify(session));
               }
-              // await login(mainUser);
               if(window?.location?.search?.includes('isFirstLogin=true') && mainUser?.user_type === "artist"){
                 window.location.href = "/onboarding";
               }else {
                 window.location.href = "/";
               }
+              // await login(mainUser);
             }
           }
         } catch (error) {
